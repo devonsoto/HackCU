3459678b65b5ed0ec9b0494082f7f6caaa474bf4
Devon Soto <deso8093@colorado.edu>
2018-02-24 13:52:23 -0700

    prototype for Reflection

--- TIMELINE.md
+++ TIMELINE.md
@@ -10,3 +10,9 @@
 12:00PM - Thought of an idea, hopefully I can finish it before Dinner(which is at 6)
 
 12:05PM - Did hackertyper for a good 5 minutes.
+
+12:58PM - Debating if I should get lunch, Dinner isn't untill 6 so imma be hungry after I eat lunch.
+
+1:16PM - My ReflectionDiary prototype is finished. Need more features, it looks lame ahha.
+
+1:50PM - Debating if I should go to the workshops right now. Yup, I'll go the one at 3.
--- day-reflection/src/App.js
+++ day-reflection/src/App.js
@@ -1,21 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+/**
+ * [App Connects all the Components together]
+ * @extends Component
+ */
+class App extends Component {  
   render() {
+    
+    //inline Stlyes
+    const AppStlye = {
+      
+    };
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={AppStlye}>
+        <ReflectionPage/>
       </div>
     );
   }
 }
+
+/**
+ * [ReflectionPage this is the outer most Component]
+ * @extends Component
+ */
+ class ReflectionPage extends Component {
+   
+   render() {
+     
+     // inline styles
+     const ReflectionPageStyle = {
+       backgroundColor: 'gray'
+     };
+     
+     return (
+       <div>
+         <Board/>
+       </div>  
+     );
+   }
+ }
+
+/**
+ * [Board this Component surrounds the text area]
+ * @extends Component
+ */
+class Board extends Component {
+  render() {
+    
+    
+    //inline styles
+    const BoardStyle = {
+      border: '1px solid black',
+      width: '900px',
+      height: '625px',
+      marginLeft: '175px',
+      marginTop: '50px',
+      textAlign: 'center',
+      outline: '5px solid black',
+      outlineOffset: '10px'
+    };
+    
+    return (
+      <div>
+        <div style={BoardStyle}>
+          <h2>Reflections of the Day</h2>
+          <ReflectionDiary/>
+        </div>  
+      </div>  
+    );
+  }
+}
+
+/**
+ * [ReflectionDiary This is the textArea that allows the user to write anything they want]
+ * @extends Component
+ */
+class ReflectionDiary extends Component {
+  render() {
+    
+    //inline styles
+    const ReflectionDiaryStyle = {
+    
+    };
+    
+    const TextAraStyle = {
+      width: '500px',
+      height: '500px',
+      backgroundColor: '#9EA09F',
+      fontSize: '12pt'
+    };
+    
+    return (
+      <div style={ReflectionDiaryStyle}>
+        <form >
+          <label>
+            <textarea placeholder="Type Away" style={TextAraStyle} type="text" />
+          </label>
+          
+        </form>
+      </div>  
+    );
+  }
+}
+
 
 export default App;
